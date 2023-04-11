0afe19108ae3a9492f42a56b3969ddaa23514948
Dustin Swecker <dustin.swecker@gmail.com>
2023-04-11 08:15:10 -0400

    switching branches, so adding what I have

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,8 @@ var questionsEl = document.getElementById("questions");
 var answersEl = document.getElementById("answers");
 var h1El = document.getElementById("h1el");
 var allLi = document.querySelectorAll("li");
-var pageNumber = questionsEl.getAttribute("data-state");
-console.log(pageNumber);
-var x;
-var setPage = questionsEl.setAttribute("data-state", "two");
-x= 2;
-console.log(pageNumber);
+
+
 
 var pArray = ["Commonly used data types DO NOT include:", "The condition in an if/else statement is enclosed with_________.", "Arrays in JavaScript can be used to store__________.", "String values must be enclosed within ____ when being assigned to variables.", "A very useful tool used during debugging and development for printing content to the debugger is:"]
 
@@ -20,8 +16,8 @@ var startQuiz = function (event) {
     startButton.style.display = 'none';
     questionsEl.textContent = pArray[0];
     questionsEl.setAttribute("style", "font-size: 40px; font-weight: bold");
-    questionsEl.dataset.state = 'two';
-    console.log(pageNumber);
+    questionsEl.setAttribute("data-state", 'two');
+    console.log(questionsEl.dataset.state);
     for(var i = 0; i<4; i++) {
     var liEl = document.createElement("li");
     liEl.setAttribute("style", "background-color: #663399; text-align: center; border-radius: 20px; margin: 10px auto; width: 250px; height: auto; border: 1px solid black");
@@ -34,7 +30,12 @@ var startQuiz = function (event) {
 
 // }
 
-// answersEl.addEventListener('click',)
+answersEl.addEventListener('click', function(event) {
+    var answerChoice = event.target;
+    if (questionsEl.dataset.state === "two") {
+        if (event.target.value === )
+    }
+})
 
 startButton.addEventListener('click', startQuiz);
 
